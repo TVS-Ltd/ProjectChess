6c7c77277ab44beea5c36329dfc1148e9487e47c
polinatonko <124461190+polinatonko@users.noreply.github.com>
2023-04-01 10:21:52 +0300

    Update entry.cpp

--- src/sources/entry.cpp
+++ src/sources/entry.cpp
@@ -1,14 +1,19 @@
 #include "entry.h"
 
-    Entry::Entry() = default;
-    Entry::Entry(ZobristHash hash, int32_t depth, uint8_t bestMoveIndex)
-    {
-        this->Hash = hash;
-        this->Depth = depth;
-        this->BestMoveIndex = bestMoveIndex;
-    }
+Entry::Entry() = default;
+Entry::Entry(ZobristHash hash, int32_t depth, uint8_t bestMoveIndex)
+{
+    this->Hash = hash;
+    this->Depth = depth;
+    this->BestMoveIndex = bestMoveIndex;
+}
 
-    bool operator<(Entry left, Entry right)
-    {
-        return (left.Hash < right.Hash);
-    }
+bool operator==(Entry left, Entry right)
+{
+    return (left.Hash == right.Hash);
+}
+
+bool operator<(Entry left, Entry right)
+{
+    return (left.Hash < right.Hash);
+}

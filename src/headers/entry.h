d2a1806b8545e8f3bd1225510dff39c4c96c8936
polinatonko <124461190+polinatonko@users.noreply.github.com>
2023-04-01 10:26:38 +0300

    Update entry.h

--- src/headers/entry.h
+++ src/headers/entry.h
@@ -6,9 +6,20 @@ public:
     Entry();
     Entry(ZobristHash hash, int32_t depth, uint8_t bestMoveIndex);
 
+    friend bool operator==(Entry left, Entry right);
     friend bool operator<(Entry left, Entry right);
 
     ZobristHash Hash;
     int32_t Depth;
     uint8_t BestMoveIndex;
+};
+
+/// Zobrist hash returned as hash function.
+
+class HashFunction {
+public:
+    size_t operator()(const Entry& t) const
+    {
+        return t.Hash.hash;
+    }
 };

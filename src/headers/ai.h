fd6ac842de0dd80f93967692cf91b9d1a2dea501
polinatonko <124461190+polinatonko@users.noreply.github.com>
2023-04-02 22:53:50 +0300

    PVS, repetitions

--- src/headers/ai.h
+++ src/headers/ai.h
@@ -1,6 +1,6 @@
 #include <future>
 #include <cmath>
-#include <unistd.h>
+//#include <unistd.h>
 #include <chrono>
 #include "position.h"
 #include "moveList.h"
@@ -15,7 +15,7 @@ class AI
 {
 public:
     AI();
-    AI(const string &openingBookPath);
+    AI(const string& openingBookPath);
 
     /**
     * Calculate the best AI move.
@@ -23,25 +23,27 @@ public:
     * @param position Current board position.
     * @param side AI side (Black/White).
     * @param minMs,maxMs Time bounds given for best move calculation.
-    * 
+    *
     * @return Best move.
     *
     * @exception This function does not throw exceptions.
     */
-    Move bestMove(const Position &position, uint8_t side, int32_t minMs, int32_t maxMs);
+    Move bestMove(const Position& position, uint8_t side, int32_t minMs, int32_t maxMs);
 
 private:
     OpeningBook openingBook;
 
-    static tuple<int32_t, Move> BestMove(const Position &position, uint8_t side, int32_t depth, TranspositionTable &TransposTable);
+    TranspositionTable TransposTable;
 
-    static tuple<int32_t, Move> alphaBetaMin(Position position, int32_t alpha, int32_t beta, int32_t depth_left, int32_t currentDepth, TranspositionTable &TransposTable);
+    static tuple<int32_t, Move> BestMove(const Position& position, uint8_t side, int32_t depth, int32_t evaluation, TranspositionTable& TransposTable);
 
-    static tuple<int32_t, Move> alphaBetaMax(Position position, int32_t alpha, int32_t beta, int32_t depth_left, int32_t currentDepth, TranspositionTable &TransposTable);
+    static tuple<int32_t, Move> alphaBetaMin(Position position, int32_t alpha, int32_t beta, int32_t depth_left, int32_t currentDepth, TranspositionTable& TransposTable);
 
-    static int32_t alphaBetaMinOnlyCaptures(const Position &position, int32_t alpha, int32_t beta, int32_t currentDepth);
+    static tuple<int32_t, Move> alphaBetaMax(Position position, int32_t alpha, int32_t beta, int32_t depth_left, int32_t currentDepth, TranspositionTable& TransposTable);
 
-    static int32_t alphaBetaMaxOnlyCaptures(const Position &position, int32_t alpha, int32_t beta, int32_t currentDepth);
+    static int32_t alphaBetaMinOnlyCaptures(const Position& position, int32_t alpha, int32_t beta, int32_t currentDepth);
+
+    static int32_t alphaBetaMaxOnlyCaptures(const Position& position, int32_t alpha, int32_t beta, int32_t currentDepth);
 
     struct Infinity
     {

c6b55487fe44b3a933585f86f87d81481237027d
polinatonko <124461190+polinatonko@users.noreply.github.com>
2023-04-21 19:33:44 +0300

    delta pruning

--- src/headers/staticEvaluator.h
+++ src/headers/staticEvaluator.h
@@ -10,19 +10,9 @@ class StaticEvaluator
 {
 public:
     static int32_t evaluate(Pieces pieces, bool whiteLongCastling, bool whiteShortCastling, bool blackLongCastling, bool blackShortCastling, bool whiteCastlingHappened, bool blackCastlingHappened, bool showDebugInfo = false);
+    static int32_t pieceMaterial(Pieces pieces, uint8_t side);
 
-private:
-    static int32_t material(Pieces pieces);
-
-    static int32_t mobility(Pieces pieces);
-    static int32_t pawnStructureDoublePawn(Pieces pieces);
-    static int32_t pawnStructureConnectedPawn(Pieces pieces);
-    static int32_t pawnStructurePawnPromotion(Pieces pieces);
-    static int32_t kingSafetyCrashedCastling(bool whiteLongCastling, bool whiteShortCastling, bool blackLongCastling, bool blackShortCastling, bool whiteCastlingHappened, bool blackCastlingHappened);
-    static int32_t kingSafetyPawnShield(Pieces pieces, bool whiteCastlingHappened, bool blackCastlingHappened);
-
-    static int32_t twoBishops(Pieces pieces);
-    static int32_t endgame(Pieces pieces, bool whiteLeading);
+    static constexpr int32_t Materials[6] = { 100, 305, 333, 563, 950, 1300 };
 
     struct Material
     {
@@ -31,7 +21,18 @@ private:
         static constexpr int32_t Bishop = 333;
         static constexpr int32_t Rook = 563;
         static constexpr int32_t Queen = 950;
+        static constexpr int32_t EndgameMat = 1300;
     };
+private:
+    static int32_t material(Pieces pieces);
+    static int32_t mobility(Pieces pieces);
+    static int32_t pawnStructureDoublePawn(Pieces pieces);
+    static int32_t pawnStructureConnectedPawn(Pieces pieces);
+    static int32_t pawnStructurePawnPromotion(Pieces pieces);
+    static int32_t kingSafetyCrashedCastling(bool whiteLongCastling, bool whiteShortCastling, bool blackLongCastling, bool blackShortCastling, bool whiteCastlingHappened, bool blackCastlingHappened);
+    static int32_t kingSafetyPawnShield(Pieces pieces, bool whiteCastlingHappened, bool blackCastlingHappened);
+    static int32_t twoBishops(Pieces pieces);
+    static int32_t endgame(Pieces pieces, bool whiteLeading);
 
     struct Mobility
     {
@@ -45,8 +46,8 @@ private:
     {
         static constexpr int32_t DoublePawn = -25;
         static constexpr int32_t ConnectedPawn = 12;
-        static constexpr std::array<int32_t, 8> DefaultPawnPromotion = {0, 0, 0, 0, 10, 20, 30, 0};
-        static constexpr std::array<int32_t, 8> PassedPawnPromotion = {0, 50, 50, 50, 70, 90, 110, 0};
+        static constexpr std::array<int32_t, 8> DefaultPawnPromotion = { 0, 0, 0, 0, 10, 20, 30, 0 };
+        static constexpr std::array<int32_t, 8> PassedPawnPromotion = { 0, 50, 50, 50, 70, 90, 110, 0 };
     };
 
     struct KingSafety

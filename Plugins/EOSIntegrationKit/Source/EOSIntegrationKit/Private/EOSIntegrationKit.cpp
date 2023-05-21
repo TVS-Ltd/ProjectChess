2c2ca91f6f80207b5d91594ae20a79b8ee8f049f
TyomaVader <tyomastarwars2017@gmail.com>
2023-05-21 19:10:23 +0300

    Switched to EOS Integration Kit for EOS Handling

---
+++ Plugins/EOSIntegrationKit/Source/EOSIntegrationKit/Private/EOSIntegrationKit.cpp
@@ -0,0 +1,20 @@
+//Copyright (c) 2023 Betide Studio. All Rights Reserved.
+
+#include "EOSIntegrationKit.h"
+
+#define LOCTEXT_NAMESPACE "FEOSIntegrationKitModule"
+
+void FEOSIntegrationKitModule::StartupModule()
+{
+}
+
+void FEOSIntegrationKitModule::ShutdownModule()
+{
+
+}
+
+
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FEOSIntegrationKitModule, EOSIntegrationKit)
